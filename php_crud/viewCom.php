c8546f809bd71748a94887ae72e7d75d9f4143c9
samaralkhamis <100211042+samaralkhamis@users.noreply.github.com>
2022-07-07 19:57:18 +0300

    php api

---
+++ php_crud/viewCom.php
@@ -0,0 +1,19 @@
+<?php
+include_once "./connect.php";
+
+$sql = "select * from comment";
+$arr =  array();
+
+$result  = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $arr[] = $row;
+    }
+} else {
+    echo "0 results";
+}
+print_r(json_encode($arr));
+$conn->close();
+
+?>
