1d3f1bee4cd6720b0ac6e960a7890c77c94df1bc
Dua-Alsafasfeh <dua.alsafasfeh@gmail.com>
2022-07-07 21:13:40 -0700

    merge with samar request

--- news/src/components/About.js
+++ news/src/components/About.js
@@ -1,42 +1,49 @@
 import React from 'react'
-function About (){
+function About(){
   return (
-    <div  >
-    	<section class="page">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-8 col-md-offset-2">
-	          <ol class="breadcrumb">
-	          	<li><a href="index.html">Home</a></li>
-	            <li class="active">About Us</li>
-	          </ol>
-						<h1 class="page-title">About Us</h1>
-						<p class="page-subtitle">We will tell you who we are</p>
-						<div class="line thin"></div>
-						<div class="page-description">
-							<h4>Global News</h4>
-							<p>
-							Global News is an news website that includes the most important daily, weekly, and monthly news, exclusive and urgent news, in addition to containing many sections, which attracts the user’s interest and meets his needs.
-							</p>
-							<p>
-              Global News  is a very powerful medium of spreading information among people.  Information is a very vital thing as we need to know what is happening around us. Also, awareness to the happenings at our surrounding helps us in better planning and decision.
-
-              Global News  are a great source of information that can be available at home. Each and everyone must ensure to imbibe the habit of reading newspapers in their lives.  It is the  Global News which ensures to provide us accurate and verified information.
-							</p>
-
-              
-					
-							<div class="question">
-								Have a question? <a href="#" class="btn btn-primary">Contact Us</a>
-							</div>
-						</div>
-					</div>
-				</div>
-            
-
-			</div>
-		</section>
-       
+    <div>
+  <section className="page">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 col-md-offset-2">
+          <ol className="breadcrumb">
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li className="active">About Us</li>
+          </ol>
+          <h1 className="page-title">About Us</h1>
+          <p className="page-subtitle">We will tell you who we are</p>
+          <div className="line thin" />
+          <div className="page-description">
+            <h4>Global News</h4>
+            <p>
+              Global News is an news website that includes the most important
+              daily, weekly, and monthly news, exclusive and urgent news, in
+              addition to containing many sections, which attracts the user’s
+              interest and meets his needs.
+            </p>
+            <p>
+              Global News is a very powerful medium of spreading information
+              among people. Information is a very vital thing as we need to know
+              what is happening around us. Also, awareness to the happenings at
+              our surrounding helps us in better planning and decision. Global
+              News are a great source of information that can be available at
+              home. Each and everyone must ensure to imbibe the habit of reading
+              newspapers in their lives. It is the Global News which ensures to
+              provide us accurate and verified information.
+            </p>
+            <div className="question">
+              Have a question?{" "}
+              <a href="#" className="btn btn-primary">
+                Contact Us
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>    
         {/* <section class="team text-center teamtech">
    <div class="container">
      <div class="header my-5">
--- news/src/components/Contact.js
+++ news/src/components/Contact.js
@@ -1,76 +1,104 @@
 import React from 'react'
 
-function Contact()  {
-  return (
-    <div>
-			<div class="container">
-				<div class="row">
-				<div class="col-md-10 top">
-	          <ol class="breadcrumb">
-	          	<li><a href="#">Home</a></li>
-	            <li class="active">Contact Us</li>
-	          </ol>
-						<h1 class="page-title">Contact Us</h1>
-						<p class="page-subtitle">We hear you</p>
-						<div class="line thin"></div>
-						<div class="page-description">
-							<div class="row">
-								<div class="col-md-6 col-sm-6">
-									<h3>Contact</h3>
-									<p>
-									Please feel free to contact us if you have any enquiry, via telephone, email or by filling out the enquiry form below.
-									</p>
-									<p>
-										Phone: <span class="bold">+962771567829</span> <br/>
-										Email: <span class="bold">globalnews@gamil.com</span>
-										<br/>
-										<br/>
-										Salah Aldeen Street<br/>
-										Jordan, Aqaba
-									</p>
-								</div>
-								<div class="col-md-6 col-sm-6">
-									<form class="row contact" id="contact-form">
-										<div class="col-md-6">
-											<div class="form-group">
-												<label>Name <span class="required"></span></label>
-												<input type="text" class="form-control" name="name" required=""/>
-											</div>
-										</div>
-										<div class="col-md-6">
-											<div class="form-group">
-												<label>Email <span class="required"></span></label>
-												<input type="text" class="form-control" name="email" required=""/>
-											</div>
-										</div>
-										<div class="col-md-12">
-											<div class="form-group">
-												<label>Subject</label>
-												<input type="text" class="form-control" name="subject"/>
-											</div>
-										</div>
-										<div class="col-md-12">
-											<div class="form-group">
-												<label>Your message <span class="required"></span></label>
-												<textarea class="form-control" name="message" required=""></textarea>
-											</div>
-										</div>
-										<div class="col-md-12">
-											<button class="btn btn-primary">Send Message</button>
-										</div>
-									</form>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-
-		
-
-
+function Contact() {
+	return (
+		<>
+		<div>
+  <div className="container">
+    <div className="row">
+      <div className="col-md-10 top">
+        <ol className="breadcrumb">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li className="active">Contact Us</li>
+        </ol>
+        <h1 className="page-title">Contact Us</h1>
+        <p className="page-subtitle">We hear you</p>
+        <div className="line thin" />
+        <div className="page-description">
+          <div className="row">
+            <div className="col-md-6 col-sm-6">
+              <h3>Contact</h3>
+              <p>
+                Please feel free to contact us if you have any enquiry, via
+                telephone, email or by filling out the enquiry form below.
+              </p>
+              <p>
+                Phone: <span className="bold">+962771567829</span> <br />
+                Email: <span className="bold">globalnews@gamil.com</span>
+                <br />
+                <br />
+                Salah Aldeen Street
+                <br />
+                Jordan, Aqaba
+              </p>
+            </div>
+            <div className="col-md-6 col-sm-6">
+              <form className="row contact" id="contact-form">
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>
+                      Name <span className="required" />
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="name"
+                      required=""
+                    />
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <label>
+                      Email <span className="required" />
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="email"
+                      required=""
+                    />
+                  </div>
+                </div>
+                <div className="col-md-12">
+                  <div className="form-group">
+                    <label>Subject</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="subject"
+                    />
+                  </div>
+                </div>
+                <div className="col-md-12">
+                  <div className="form-group">
+                    <label>
+                      Your message <span className="required" />
+                    </label>
+                    <textarea
+                      className="form-control"
+                      name="message"
+                      required=""
+                      defaultValue={""}
+                    />
+                  </div>
+                </div>
+                <div className="col-md-12">
+                  <button className="btn btn-primary">Send Message</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  </div>
+</div>
+
+		</>
+	)
 }
 
 export default Contact
